75ae8c07fd62feb1e92c2d106685dc38cf6eabc6
Jenna Miles <jennamiles@Jennas-MacBook-Pro.local>
2019-12-11 22:54:04 -0700

    setting up project baseline

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// pseudo code
+
+// sidebar inputs user text, queries api, and adds div to search sidebar
+
+// current city div displays temp, humidity, wind speed, and UV index
+
+// 5 day forecast displays the date, the temp, the humidity, and an icon
