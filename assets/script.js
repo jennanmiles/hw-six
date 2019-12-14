7b6591314eb239081bc000cb9cf34c8fdcefc2d2
Jenna Miles <jennamiles@Jennas-MacBook-Pro.local>
2019-12-14 09:50:39 -0700

    adding search portion

--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,71 @@
 // pseudo code
 
-// sidebar inputs user text, queries api, and adds div to search sidebar
+$('#search').click(function() {
+    let userCity = $('#searchField').val();
+    console.log(userCity);
 
+    // sidebar inputs user text, queries api, and adds div to search sidebar
+    $('.recentSearch').append('<div class="textlabel">' + userCity +'</div>');
 
-// current city div displays temp, humidity, wind speed, and UV index
-//let queryUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityInput + '&APPID=0a2917cfe155f518d8a07dd10675329a';
-let queryUrl = 'http://api.openweathermap.org/data/2.5/weather?q=tucson&APPID=0a2917cfe155f518d8a07dd10675329a&units=imperial';
+    // clear user input on click?
 
-$.ajax({
-    url: queryUrl,
-    method: 'GET',
-}).then(function(response) {
-    //console.log(response);
-    // city name
-    $('#cityName').html(response.name);
-    // temp
-    $('#cityTemp').html('Temperature: ' + response.main.temp + '°F');
-    // humidity
-    $('#cityHumidity').html('Humidity: ' + response.main.humidity + '%');
-    // wind speed
-    $('#cityWind').html('Wind Speed: ' + response.wind.speed + ' MPH');
-    // uv index
-    
-});
+    // current city div displays temp, humidity, wind speed, and UV index
+    let queryUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + userCity +'&APPID=0a2917cfe155f518d8a07dd10675329a&units=imperial';
+    // let lat = queryUrl.coord.lat
+    // let lon = queryUrl.coord.lon
+    // console.log(lat);
+    // console.log(lon);
 
-// uv index query, date query
-let queryUrl2 = 'http://api.openweathermap.org/data/2.5/uvi?appid=0a2917cfe155f518d8a07dd10675329a&lat=32.22&lon=-110.93'
+    $.ajax({
+        url: queryUrl,
+        method: 'GET',
+    }).then(function(response) {
+        console.log(response);
+        // city name
+        $('#cityName').html(response.name);
+        // temp
+        $('#cityTemp').html('Temperature: ' + response.main.temp + '°F');
+        // humidity
+        $('#cityHumidity').html('Humidity: ' + response.main.humidity + '%');
+        // wind speed
+        $('#cityWind').html('Wind Speed: ' + response.wind.speed + ' MPH');
+        
+    });
 
-$.ajax({
-    url: queryUrl2,
-    method: 'GET',
-}).then(function(response) {
-    //console.log(response);
-    // date
-    $('#cityDate').html('Date: ' + response.date_iso);
-    // uv index
-    $('#cityUV').html('UV Index: ' + response.value);
-})
+    // uv index query, date query
+    let queryUrl2 = 'http://api.openweathermap.org/data/2.5/uvi?appid=0a2917cfe155f518d8a07dd10675329a&lat=32.22&lon=-110.93'
 
-// 5 day forecast displays the date, the temp, the humidity, and an icon
-let queryUrl3 = 'http://api.openweathermap.org/data/2.5/forecast?appid=0a2917cfe155f518d8a07dd10675329a&q=tucson&units=imperial'
+    $.ajax({
+        url: queryUrl2,
+        method: 'GET',
+    }).then(function(response) {
+        //console.log(response);
+        // date
+        $('#cityDate').html('Date: ' + response.date_iso);
+        // uv index
+        $('#cityUV').html('UV Index: ' + response.value);
+    })
 
-$.ajax({
-    url: queryUrl3,
-    method: 'GET',
-}).then(function(response) {
-    let arraylen = response.list;
-    // getting 5 day forecast
-    let newArray = arraylen.slice(0,5);
+    // 5 day forecast displays the date, the temp, the humidity, and an icon
+    let queryUrl3 = 'http://api.openweathermap.org/data/2.5/forecast?appid=0a2917cfe155f518d8a07dd10675329a&q=' + userCity + '&units=imperial'
 
-    for (let i = 0; i < newArray.length; i++) {
-        let temp = newArray[i].main.temp;
-        console.log(temp);
-        let card = $('.forecastCardWrapper').append('<div class="forecastCard"><div class="temp"></div></div>');
-        $('.temp').html(temp);
-    }
+    $.ajax({
+        url: queryUrl3,
+        method: 'GET',
+    }).then(function(response) {
+        let arraylen = response.list;
+        // getting 5 day forecast
+        let newArray = arraylen.slice(0,5);
+        //console.log(newArray);
+
+        for (let i = 0; i < newArray.length; i++) {
+            //let temp = newArray[i].main.temp;
+        console.log(newArray);
+            $('.forecastCardWrapper').append('<div class="forecastCard"></div>');
+            $('.forecastCard').append('<div class="temp"></div>').html(newArray[i].main.temp);
+            $('.forecastCard').append('<div class="humidity"></div>').html(newArray[i].main.humidity);
+        }
+    })
+
+// close click function
 })
